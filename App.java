dc8b88408374949c1bf5758bedb68f935dc29a2d
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-10 17:43:27 +0530

    Initial commit with sample App

---
+++ App.java
@@ -0,0 +1,18 @@
+class App {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println();
+
+    Person p = new Person();
+
+    p.name = "Ram";
+    p.gender = "male";
+    p.age = 1;
+
+    System.out.println(p.name);
+    System.out.println(p.gender);
+    System.out.println(p.age);
+  }
+
+}
---
+++ Person.java
@@ -0,0 +1,6 @@
+class Person {
+
+  String name, gender;
+  int age;
+
+}
